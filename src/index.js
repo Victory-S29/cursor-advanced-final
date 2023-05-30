25eead0074ddf3e84b78e35dd782c311725286dc
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-05-30 16:54:26 +0300

    changed path

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/cursor-advanced/final-project/build">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
