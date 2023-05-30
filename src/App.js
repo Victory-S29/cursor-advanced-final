d1f78d5549406eb4153510b9489ee7fb7a01c1e9
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-05-30 19:41:24 +0300

    test img

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Button from './style/Button';
 import MainPage from './pages/main-page/MainPage';
 import Error from './pages/error/Error';
 import ProjectDetails from './pages/project-detail/ProjectDetails';
-
+import image from "./imgs/Timer/timer1.png"
 
 function App() {
   const [lang, setLanguage] = useState("en")
@@ -27,6 +27,7 @@ function App() {
       <Button onClick={changeLanguage}>
         {lang === "en" ? "EN" : "UA"}
       </Button>
+      <img src={image}></img>
       <Routes>
         <Route index element={<MainPage />} />
         <Route path='*' element={<Error />} />
