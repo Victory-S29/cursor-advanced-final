c2cae26560268c541a6413cc2d130c14263e2a08
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-06-02 12:25:49 +0300

    fixed typing

--- src/text.js
+++ src/text.js
@@ -21,7 +21,7 @@ const text = {
             LinkedIn:"https://www.linkedin.com/in/%D0%B2%D1%96%D0%BA%D1%82%D0%BE%D1%80%D1%96%D1%8F-%D1%81%D0%B0%D0%BD%D1%96%D0%BD%D0%B0-156906257/",
             Gmail: "viktoriia.sanina.2917@gmail.com",
             Phone: "066-378-9986",
-            Place:"ВІддалено/Київ",
+            Place:"Віддалено/Київ",
         }
     },
     en: {
